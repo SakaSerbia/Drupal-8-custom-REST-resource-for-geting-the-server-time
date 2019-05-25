1789f1b5fa042735145770b741ea239e3fc7edf1
Stefan T <SakaSerbia@users.noreply.github.com>
2019-05-25 21:01:04 +0200

    Add files via upload

---
+++ time_service/src/Plugin/rest/resource/TimeService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\time_service\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Drupal\Core\Cache\CacheableResponseInterface;
+use Drupal\Core\Cache\CacheableResponseTrait;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "time_service",
+ *   label = @Translation("Time Service"),
+ *   uri_paths = {
+ *     "canonical" = "/rest/time_service"
+ *   }
+ * )
+ */
+class TimeService extends ResourceBase implements CacheableResponseInterface {
+
+    use CacheableResponseTrait;
+
+    /**
+     * Responds to entity GET requests.
+     * @return \Drupal\rest\ResourceResponse
+     */
+    public function get() {
+        $build =  [
+            '#cache' => [
+                'max-age' => 0,
+            ]
+        ];
+        
+        $message = [
+            'year' => date("Y"),
+            'month' => date("m"),
+            'day' => date("d"),
+            'hour' => date("H"),
+            'minute' => date("i"),
+            'second' => date("s"),
+            /**
+            'time' => date("Y-m-d H:i:s"),
+            'timezone' => date_default_timezone_get(),
+             * */
+        ];
+
+        $response = new ResourceResponse($message);
+        $response->addCacheableDependency($build);
+        return $response;
+    }
+}
